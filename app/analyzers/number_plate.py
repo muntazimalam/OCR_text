0efa63b7ef1dd58e664a4ec4ce699e472a9e405f
Muntazim Alam <muntazimalam@users.noreply.github.com>
2026-08-09 22:54:48 +0530

    fix(number_plate): tighten EU/Intl regex to prevent false positives on short street banner words

--- app/analyzers/number_plate.py
+++ app/analyzers/number_plate.py
@@ -11,7 +11,7 @@ SPECIFIC_PLATE_PATTERNS = [
     # European / UK Standard (e.g., AB12CDE, ABC123, B1234AB)
     (re.compile(r"^[A-Z]{2}[0-9]{2}[A-Z]{3}$"), 0.90, "EU/UK Standard"),
     (re.compile(r"^[A-Z]{3}[0-9]{3}$"), 0.88, "EU Standard"),
-    (re.compile(r"^[A-Z]{1,2}[0-9]{1,4}[A-Z]{1,3}$"), 0.88, "EU/Intl Standard"),
+    (re.compile(r"^[A-Z]{1,2}[0-9]{2,4}[A-Z]{2,3}$"), 0.88, "EU/Intl Standard"),
     # US / North American Standard (e.g., 7ABC123, 1ABC234, ABC1234)
     (re.compile(r"^[0-9][A-Z]{3}[0-9]{3}$"), 0.88, "US Standard"),
     (re.compile(r"^[A-Z]{3}[0-9]{4}$"), 0.88, "US Standard"),
